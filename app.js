30ca0eb7d2d65621c659c21fe5d6042084b41305
kauansddsdela <kauanacalmeida17@gmail.com>
2023-08-18 10:45:43 -0300

    hehe

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const app = express()
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/html/index.html")
+})
+
+app.get("/contato", function(req, res){
+    res.send("Página de contato")
+})
+
+app.get("/produtos/:item/:quantidade", function(req, res){
+    res.send("item: " + req.params.item + "<br> Quantidae: " + req.params.quantidade)
+})
+
+app.listen(8081, function(){
+    console.log("Servidor rodando na porta 8081")
+})
